f001072574afae79efd2aa1e844840ef5339029e
Fernanda <fernanda.winter@dbserver.com.br>
2020-03-12 11:18:17 -0300

    Add project files.

---
+++ DemoStore/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
